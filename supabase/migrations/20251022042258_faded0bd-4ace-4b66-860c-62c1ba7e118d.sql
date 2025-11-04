36d5fdc38dedcd149b0bbddc74f15f0ed4c86064
CelestialBrain <50072826+CelestialBrain@users.noreply.github.com>
2025-11-05 02:08:17 +0800

    Create 20251022042258_faded0bd-4ace-4b66-860c-62c1ba7e118d.sql

---
+++ supabase/migrations/20251022042258_faded0bd-4ace-4b66-860c-62c1ba7e118d.sql
@@ -0,0 +1,3 @@
+-- Trigger type regeneration by touching the schema
+-- This comment change will force types to sync without altering data
+COMMENT ON TABLE public.users IS 'User profiles and authentication data';
